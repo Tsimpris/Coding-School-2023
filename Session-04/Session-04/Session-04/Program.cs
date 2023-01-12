403721575fc492b8d2e737e654cd32ef838048d0
Tsimpris Giannis <johntsimpris@gmail.com>
2023-01-12 14:29:39 +0200

    Adding comments

--- Session-04/Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Session-04/Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Session_04;
 
-//
+//Task One
 Taskone helloName = new Taskone();
 
 Console.WriteLine(helloName.ErvthmaEna());
+
+//Task Two
 
 Console.ReadLine();
