816f7f30beec98ffb524b99200bcf81db97f113e
Tsimpris Giannis <johntsimpris@gmail.com>
2023-01-12 14:17:40 +0200

    Class TaskOne, call the class with helloName object and runs the method ErvthmaEna()

---
+++ Session-04/Session-04/Session-04/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Session_04;
+
+//
+Taskone helloName = new Taskone();
+
+Console.WriteLine(helloName.ErvthmaEna());
+
+Console.ReadLine();
---
+++ Session-04/Session-04/Session-04/Taskone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04
+{
+    public class Taskone
+    {
+        public string ErvthmaEna()
+        {
+
+            // 1. Write a C# function to print Hello and your name in the same line. 
+
+            string greeting = "Hello";
+            string name = "Giannis";
+
+            return greeting + " " + name;
+
+        }
+    }
+}
