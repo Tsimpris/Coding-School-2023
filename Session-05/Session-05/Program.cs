dcdb0048eb317c32c4df5d2e7f9c3dda0eb1d2d5
Tsimpris Giannis <johntsimpris@gmail.com>
2023-01-14 17:22:32 +0200

    Class builds correctly now

---
+++ Session-05/Session-05/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Session_05;
+
+//Task One
+ReverseString myName = new ReverseString();
+Console.WriteLine(myName.Returnname());
---
+++ Session-05/Session-05/ReverseString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+    internal class ReverseString
+    {
+        public string Returnname()
+        {
+            string str = "", reverse = "";
+            int Length = 0;
+            Console.WriteLine("Enter your name:");
+            str = Console.ReadLine();
+            Length = str.Length - 1;
+            while (Length >= 0)
+            {
+                reverse = reverse + str[Length];
+                Length--;
+            }
+            Console.WriteLine("Your name reversed is:");
+            return reverse;
+        }
+
+    }
+}
