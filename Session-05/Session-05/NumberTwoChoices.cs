c91b38dee562f98cf23b3e867716f3456bfba86c
Tsimpris Giannis <johntsimpris@gmail.com>
2023-01-14 18:13:03 +0200

    Task 2 from Session 05 given exercise, done

--- Session-05/Session-05/NumberTwoChoices.cs
+++ Session-05/Session-05/NumberTwoChoices.cs
@@ -8,5 +8,36 @@ namespace Session_05
 {
     internal class NumberTwoChoices
     {
+        public int TwoChoises()
+        {
+            Console.WriteLine("Please enter an integer number:");
+            var numString = Console.ReadLine();
+            int n = Convert.ToInt32(numString);
+            int sum = 0;
+            int product = 1;
+            int fanswer = 0;
+            Console.WriteLine("Please enter <SUM> for Addition or <FACT> for factorial on your given number:");
+            string answer = Console.ReadLine();
+            if (answer == "sum")
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    sum = sum + i;
+                }
+                Console.WriteLine("The addition of your given number is:");
+                fanswer = sum;
+            }
+            else if (answer == "fact")
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    product = product * i;
+                }
+                Console.WriteLine("The factorial of your given number is:");
+                fanswer = product;
+            }
+            return fanswer;
+
+        }
     }
 }
--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -4,3 +4,7 @@ using Session_05;
 //Task One
 ReverseString myName = new ReverseString();
 Console.WriteLine(myName.Returnname());
+
+//Task Two
+NumberTwoChoices number = new NumberTwoChoices();
+Console.WriteLine(number.TwoChoises());
