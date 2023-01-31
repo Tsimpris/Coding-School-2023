ae4872d6ea647c5332adf77ff6965693666e55e4
Tsimpris Giannis <johntsimpris@gmail.com>
2023-01-31 18:53:59 +0200

    Connecting to local db and make entity configurations

---
+++ EF.Course/EF.Course.Model/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Course.Model {
+    public class Todo {
+        public Todo(string title) {
+            Title = title;
+
+        }
+
+        public string Title { get; set; }
+
+        public bool Finished { get; set; }
+    }
+}
---
+++ EF.Course/EF.Course.Orm/AppContext.cs
@@ -0,0 +1,21 @@
+﻿using EF.Course.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Course.Orm {
+    public class TodoContext : DbContext {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-3RO8C5RV\\SQLEXPRESS;Initial Catalog=TodoDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
