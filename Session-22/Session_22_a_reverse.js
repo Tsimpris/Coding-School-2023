f16916ecbc4552bcdf89b9d57f1e51ca1723f42a
Tsimpris Giannis <122268373+Tsimpris@users.noreply.github.com>
2023-02-07 17:17:01 +0200

    Add files via upload
    Session 22 exercise

---
+++ Session-22/Session_22_a_reverse.js
@@ -0,0 +1,16 @@
+function reverseString(str) {
+
+    var splitString = str.split(""); 
+    
+    var reverseArray = splitString.reverse(); 
+    
+    
+    var joinArray = reverseArray.join(""); 
+    
+    
+    return joinArray;
+}
+ 
+reverseString("hello world");
+
+console.log(joinArray);
---
+++ Session-22/Session_22_b_palindrome.js
@@ -0,0 +1,22 @@
+function checkPalindrome(string) {
+
+    // convert string to an array
+    const arrayValues = string.split('');
+
+    // reverse the array values
+    const reverseArrayValues = arrayValues.reverse();
+
+    // convert array to string
+    const reverseString = reverseArrayValues.join('');
+
+    if(string == reverseString) {
+        console.log('It is a palindrome');
+    }
+    else {
+        console.log('It is not a palindrome');
+    }
+}
+
+const string = prompt('Enter a string: ');
+
+checkPalindrome(string);
